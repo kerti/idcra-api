330d41586e427bc9a0b90ad7e2c28b2eae7f1612
Raditya Kertiyasa <rad@salestock.id>
2018-09-14 09:10:23 +0700

    Preflight Checks Episode N (#10)

--- handler/context.go
+++ handler/context.go
@@ -1,12 +1,17 @@
 package handler
 
 import (
-	"golang.org/x/net/context"
 	"net/http"
+
+	"golang.org/x/net/context"
 )
 
 func AddContext(ctx context.Context, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization,Content-Type")
 		h.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
