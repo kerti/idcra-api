bca024f8fae61fdaa2a65df46d325b646c97c1e2
Raditya Kertiyasa <rad@salestock.id>
2018-09-14 08:24:51 +0700

    Add Access-Control-Allow-Headers: Authorization header for preflights.

--- handler/graphql.go
+++ handler/graphql.go
@@ -28,6 +28,7 @@ func (h *GraphQL) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Authorization")
 
 	// check for empty body
 	body, err := ioutil.ReadAll(r.Body)
