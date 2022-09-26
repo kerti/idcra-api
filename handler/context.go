106dce5a712a260d073e8255f6521ad169ef6ca9
Raditya Kertiyasa <adit.kerti@gmail.com>
2022-09-26 20:23:04 +0700

    Fix CORS preflight in handler.

--- handler/context.go
+++ handler/context.go
@@ -10,7 +10,7 @@ func AddContext(ctx context.Context, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "POST,OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Authorization,Content-Type")
 		h.ServeHTTP(w, r.WithContext(ctx))
 	})
